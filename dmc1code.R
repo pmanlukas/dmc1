5e814aefbe1df415249b738ef5c60f18e0b2ae38
pmanlukas <lukas.pollmann@tum.de>
2017-01-01 16:05:12 +0100

    added our test dataset to our r script and also altered some of its attributes

--- dmc1code.R
+++ dmc1code.R
@@ -15,11 +15,13 @@ set.seed(42) # do NOT CHANGE this seed
 # 2. Load & Explore the Training Data Set
 setwd("C:/Users/lukaspman/Documents/BaR")
 training_data = read.csv("trainingDMC1.csv", sep=",")
+test_data = read.csv("testDMC1.csv", sep=",")
 ######################################################
 
 ##fix the datatypes of the data set
 #create new dataframe to do altering on the attributes, without damaging our original df
 t1data = training_data
+te1data = test_data
 
 #inspect the data set
 str(t1data)
@@ -29,15 +31,22 @@ table(t1data$Job)
 #fix Default
 table(t1data$Default)
 t1data$Default = factor(t1data$Default, labels = c("no", "yes"))
+te1data$Default = factor(te1data$Default, labels = c("no", "yes"))
+
 
 #fix HHInsurance
 t1data$HHInsurance = factor(t1data$HHInsurance, labels = c("no", "yes"))
+te1data$HHInsurance = factor(te1data$HHInsurance, labels = c("no", "yes"))
+
 table(t1data$HHInsurance)
 
 #fix CarLoan
 table(t1data$CarLoan)
 t1data$CarLoan = factor(t1data$CarLoan, labels = c("no", "yes"))
+te1data$CarLoan = factor(te1data$CarLoan, labels = c("no", "yes"))
+
 
 #fix CarInsurance
 table(t1data$CarInsurance)
 t1data$CarInsurance = factor(t1data$CarInsurance, labels = c("no", "yes"))
+te1data$CarInsurance = factor(te1data$CarInsurance, labels = c("no", "yes"))
